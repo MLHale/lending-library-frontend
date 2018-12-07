a23ae0581d3ac472cedb56f236ad571ae8999d0a
Michael <30883926+mgalde@users.noreply.github.com>
2018-12-06 18:14:45 -0600

    Added Styles

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,32 @@
 import Controller from '@ember/controller';
+import Ember from 'ember';
 
 export default Controller.extend({
+  actions: {
+    /**
+      Open external links dispatched from the nav menu
+    **/
+    externalLink(item){
+      window.open(item.route);
+    },
+
+    /**
+      Set of functions related to opening and closing menu options
+    **/
+    openDialog(event) {
+      this.set('dialogOrigin', Ember.$(event.currentTarget));
+      this.set('showDialog', true);
+    },
+    closeDialog(result) {
+      this.set('result', result);
+      this.set('showDialog', false);
+    },
+
+    /**
+      Pass logout action onward to auth service
+    **/
+    logout(){
+      this.get('auth').logout();
+    }
+  }
 });
