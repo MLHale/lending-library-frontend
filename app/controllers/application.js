732c259fa85af3519f850fac7d560dd3c6fdb69f
Michael <30883926+mgalde@users.noreply.github.com>
2018-12-06 18:52:45 -0600

    updates to style sheets

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,32 +1,4 @@
 import Controller from '@ember/controller';
-import $ from 'jquery';
 
 export default Controller.extend({
-  actions: {
-    /**
-      Open external links dispatched from the nav menu
-    **/
-    externalLink(item){
-      window.open(item.route);
-    },
-
-    /**
-      Set of functions related to opening and closing menu options
-    **/
-    openDialog(event) {
-      this.set('dialogOrigin', $(event.currentTarget));
-      this.set('showDialog', true);
-    },
-    closeDialog(result) {
-      this.set('result', result);
-      this.set('showDialog', false);
-    },
-
-    /**
-      Pass logout action onward to auth service
-    **/
-    logout(){
-      this.get('auth').logout();
-    }
-  }
 });
--- app/initializers/auth-manager.js
+++ app/initializers/auth-manager.js
@@ -1,10 +1,7 @@
-export function initialize(application) {
-	application.inject('route', 'auth', 'service:auth-manager');
-	application.inject('controller', 'auth', 'service:auth-manager');
-	application.inject('component', 'auth', 'service:auth-manager');
+export function initialize(/* application */) {
+  // application.inject('route', 'foo', 'service:foo');
 }
 
 export default {
-	name: 'auth-manager',
-	initialize: initialize
+  initialize
 };
