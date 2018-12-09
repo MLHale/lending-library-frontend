b173b0e34836caa922f57bcdd8e31ebc4808e6c8
Michael <30883926+mgalde@users.noreply.github.com>
2018-12-09 14:02:51 -0600

    API Corrections
    API Errors

--- app/services/auth-manager.js
+++ app/services/auth-manager.js
@@ -1,24 +1,24 @@
 import Service from '@ember/service';
-import bool from '@ember/object/computed';
-import $ from 'jquery';
+//import bool from '@ember/object/computed';
+//import $ from 'jquery';
 
 export default Service.extend({
-  accessToken: null,
+  //accessToken: null,
 
-  authenticate(login, password) {
-    return $.ajax({
-      method: "POST",
-      url: "/token",
-      data: { username: login, password: password }
-    }).then((result) => {
-      this.set('accessToken', result.access_token);
-    });
-  },
-
-  invalidate() {
-    this.set('accessToken', null);
-  },
-
-  isAuthenticated: bool('accessToken')
+//  authenticate(login, password) {
+//    return $.ajax({
+//      method: "POST",
+//      url: "/token",
+//      data: { username: login, password: password }
+//    }).then((result) => {
+//      this.set('accessToken', result.access_token);
+//    });
+//  },
+//
+//  invalidate() {
+//    this.set('accessToken', null);
+//  },
+//
+//  isAuthenticated: bool('accessToken')
 
 });
