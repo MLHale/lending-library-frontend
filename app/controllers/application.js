814958303bc7cabac2dddb75ed0c86e8c0475630
mellonb <bmellon@unomaha.edu>
2018-12-13 10:29:23 -0600

    add delete to column

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,10 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+  deleteRecord: function(record) {
+    var model = this.get('model');
+    this.set('model', model.without(record));
+  }
+}
 });
