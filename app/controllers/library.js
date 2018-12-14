1594c9057ee57192d7214631af72738c869d7b7e
Mounika Addagada <maddagada@unomaha.edu>
2018-12-13 20:01:25 -0600

    Added quantity retrieve method

--- app/controllers/library.js
+++ app/controllers/library.js
@@ -8,7 +8,27 @@ export default Controller.extend({
     return 'rpi2b.jpg';
   }),
 
-  
+  actions: {
+    getitemcount(){
+      //Loop through the objects and get count for each object and update objlist
+      this.objlist.forEach(function(ite){
+      Ember.$.ajax({
+        //url can change, we just need to update the url
+        url:'/api/items/count?partname='+ite.name,
+        type:"GET",
+        contentType:"application/json",
+        dataType:"json",
+        success: function(response){
+          ite.qty=response.count;
+        },
+        error: function(responsse) {
+          // Do error handling
+        }
+
+        });
+    });
+  }
+},
 
 //When we fecth items, just call this.actions.getitemcount(), this will update quantity values in objlist.
 
