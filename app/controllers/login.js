677ee9cc21f9b82e3e0e8973f51f4fa78879758f
jlevyTUM <jbl14985@gmail.com>
2019-04-08 19:18:43 -0500

    modified:   app/controllers/login.js 	modified:   app/services/auth-manager.js 	modified:   app/templates/login.hbs

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,26 @@
+/**
+ * @Author: Matthew Hale <matthale>
+ * @Date:   2018-05-17T15:53:43-05:00
+ * @Email:  mlhale@unomaha.edu
+ * @Filename: login.js
+ * @Last modified by:   matthale
+ * @Last modified time: 2018-05-17T17:10:52-05:00
+ * @Copyright: Copyright (C) 2018 Matthew L. Hale
+ */
+
+
+
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+	auth: service('auth-manager'),
+	actions:{
+		login: function(){
+			this.get('auth').login();
+		},
+		logout: function(){
+			this.get('auth').logout();
+		}
+	}
 });
--- app/services/auth-manager.js
+++ app/services/auth-manager.js
@@ -1,4 +1,181 @@
+/**
+ * @Author: Matthew Hale <mlhale>
+ * @Date:   2018-02-22T22:39:20-06:00
+ * @Email:  mlhale@unomaha.edu
+ * @Filename: auth-manager.js
+ * @Last modified by:   mlhale
+ * @Last modified time: 2018-12-04T00:55:55-06:00
+ * @Copyright: Copyright (C) 2018 Matthew L. Hale
+ */
+
+
+
 import Service from '@ember/service';
+import { inject as service } from '@ember/service';
+import $ from 'jquery';
+import { later } from '@ember/runloop';
 
 export default Service.extend({
+	store: service('store'),
+	router: service(),
+
+	//field vars
+	username: '',
+	password: '',
+	remember: false,
+	errorMsg: '',
+
+	//stored data
+	user: null,
+	profile: null,
+	isLoggedIn: false,
+	shownav: true,
+
+	/**
+		Authenticates against session endpoint on backend (at /api/session)
+	**/
+	login: function(eyetracker=true, callback){
+		console.log('Logging in:');
+
+		//retrieve field data
+		var username = this.get('username');
+		var password = this.get('password');
+		var remember = this.get('remember');
+		console.log('1');
+		var data = {
+			'username': username,
+			'password': password};
+		console.log('2');
+		var auth = this;
+		console.log('3');
+		//make api request
+		$.post('/api/session/', data, function(response){
+			console.log('4');
+			if(response.data.isauthenticated){
+				//success
+				console.log('Login POST Request to /api/session/ was successful.');
+
+
+				if(remember){
+					//save username and pass to local storage
+					localStorage.setItem('remember', true);
+					localStorage.setItem('username', auth.get('username'));
+					localStorage.setItem('password', auth.get('password'));
+				}
+				else{
+					//failure
+					localStorage.removeItem('remember');
+					localStorage.removeItem('username');
+					localStorage.removeItem('password');
+				}
+				console.log('5');
+				auth.get('store').findRecord('profile', response.data.profileid /*, {include: 'experimentsessions,user,experimentsessions.subjectsessions'}*/).then(
+					function(profile){
+						console.log('6');
+						auth.set('user',profile.get('user'));
+						// transition after the profile is loaded
+						auth.set('profile',profile);
+						auth.set('password', '');
+						if (callback){
+							callback();
+						}
+						else {
+							if(profile.get('roles').admin) {
+								auth.get('router').transitionTo('home');
+							} else {
+								auth.get('router').transitionTo('home');
+							}
+						}
+					}
+				);
+				console.log(7);
+				auth.set('isLoggedIn', true);
+
+			} else{
+				//errors
+				console.log('Login POST Request to /api/session/ was unsuccessful.');
+				auth.set('errorMsg', response.data.message);
+			}
+		});
+
+	},
+	/**
+		De-authenticates against session endpoint on backend (at /api/session)
+	**/
+	logout: function(){
+		console.log('Logging out');
+		var auth = this;
+		$.ajax({url: '/api/session/', type: 'DELETE'}).then(
+			function(response){
+				console.log('Logout DELETE Request to /api/session/ was successful:' + response);
+				auth.set('isLoggedIn', false);
+				auth.set('errorMsg', '');
+				auth.set('username', '');
+				auth.set('user', null);
+				auth.set('profile', null);
+
+				if(localStorage.remember) {
+					auth.set('remember', localStorage.remember);
+					auth.set('username', localStorage.username);
+					auth.set('password', localStorage.password);
+				}
+
+				auth.get('router').transitionTo('login');
+
+			}
+		);
+	},
+	/**
+		called whenever the application loads to initialize any stored session/local variables
+	**/
+	init: function(){
+		this._super();
+		var auth = this;
+
+		let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+		auth.set('isChrome', isChrome);
+
+		//handle session and local variable loading
+		auth.set('remember', localStorage.remember);
+		if(auth.get('remember')){
+			auth.set('username', localStorage.username);
+			auth.set('password', localStorage.password);
+		}
+
+
+		//check to see if the user is logged into the API
+		$.get('/api/session', function(response){
+			if(response.data.isauthenticated){
+				//success
+				console.log('The user: \''+response.data.username+'\' is currently logged in.');
+				auth.get('store').findRecord('profile', response.data.profileid /*, {include: 'experimentsessions,user,experimentsessions.subjectsessions'}*/).then(
+					function(profile){
+						auth.set('user',profile.get('user'));
+						auth.set('profile',profile);
+						if(auth.get('profile.roles.annon')){
+							auth.set('isannon', true);
+						}
+						if (auth.get('router._router.currentPath')==='login'){
+							// transition if on the login page
+							if(profile.get('roles').admin) {
+								auth.get('router').transitionTo('home');
+							} else {
+								auth.get('router').transitionTo('home');
+							}
+						}
+					}
+				);
+				auth.set('isLoggedIn', true);
+				auth.set('password', '');
+
+			}
+			else{
+				//errors
+				console.log('The user is not currently logged in.');
+			}
+		});
+
+
+	}
 });
