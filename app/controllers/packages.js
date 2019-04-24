06e08bd3e5fc3b79d015f42170bee96855cdff28
nsharper <nharper@unomaha.edu>
2019-04-24 06:43:23 -0500

    implemented "Add to cart" functionality for packages

--- app/controllers/packages.js
+++ app/controllers/packages.js
@@ -1,4 +1,25 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions:{
+    add: function(pkg, qty){
+      var contr = this;
+      var cart = this.get('auth.profile.cart');
+      console.log('Quantity: ' + qty)
+      console.log("You've called the \"add\" function for packages.")
+      console.log(pkg.name)
+
+      for (var i = 0; i < qty; i++){
+        pkg.get('packageitemtypequantities').forEach(function(packageitemtypequantity){
+          let newitem = contr.get('store').createRecord('cartitemtypequantity', {
+            cart: cart,
+            itemtype: packageitemtypequantity.get('itemtype'),
+            quantity: packageitemtypequantity.get('quantity')
+          });
+
+          newitem.save();
+        });
+      }
+    }
+  }
 });
