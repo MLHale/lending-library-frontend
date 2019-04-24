e33ca744d27e509ea4580a3204523625b3b92450
nsharper <nharper@unomaha.edu>
2019-04-24 15:47:32 -0500

    controllers/library.js now updates existing records when they exist; cartitemtypequantity and packageitemtypequantity now have their quantity field set as 'number'; the type for the input helper on templates/library.hbs was changed from 'text' to 'number'

--- app/controllers/library.js
+++ app/controllers/library.js
@@ -3,20 +3,30 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 
   actions:{
-    add: function(itemtype, quantity){
-      let cart = this.get('auth.profile.cart');
+    add: function(itemtype){
+      var cart = this.get('auth.profile.cart');
       console.log(cart);
       console.log("You've called the \"add\" function.");
       console.log('Itemtype: ' + itemtype);
-      console.log('Quantity: ' + quantity);
+      console.log('Quantity: ' + itemtype.get('quantity'));
 
-      let newitem = this.get('store').createRecord('cartitemtypequantity', {
-        cart: cart,
-        itemtype: itemtype,
-        quantity: quantity
-      });
+      var found = cart.get('cartitemtypequantities').findBy('itemtype.name', itemtype.get('name'))
 
-      newitem.save();
+      if (found){
+        console.log('Itemtype exists in cart, updating quantity...')
+
+        found.set('quantity', found.get('quantity') + Number(itemtype.get('quantity')))
+        found.save()
+      } else{
+        console.log('Itemtype does not exist in cart, creating new record...')
+        let newitem = this.get('store').createRecord('cartitemtypequantity', {
+          cart: cart,
+          itemtype: itemtype,
+          quantity: itemtype.get('quantity')
+        });
+
+        newitem.save();
       }
+    }
   }
 });
--- app/models/cartitemtypequantity.js
+++ app/models/cartitemtypequantity.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   cart: DS.belongsTo('cart'),
   itemtype: DS.belongsTo('itemtype'),
-  quantity: DS.attr(),
+  quantity: DS.attr('number'),
 });
