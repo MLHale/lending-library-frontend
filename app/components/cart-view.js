c44ba0825d109627201bc97538b7eb9f867d53ad
nsharper <nharper@unomaha.edu>
2019-04-24 02:58:06 -0500

    components/cart-view.js contains order, remove, and empty actions; controllers/library.js contains add function; removed unneeded text from templates/cart.hbs; added functionality and content display for components/cart-view.hbs; minor changes to templates/library.hbs; minor changes to templates/packages.hbs

--- app/components/cart-view.js
+++ app/components/cart-view.js
@@ -1,4 +1,23 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions:{
+    order: function(){
+      console.log("You've called the \"order\" function.");
+    },
+    remove: function(itemtype){
+      console.log("You've called the \"remove\" function to remove (" +
+        itemtype.quantity + ") " + itemtype.get('itemtype.name'));
+      itemtype.deleteRecord();
+      itemtype.save();
+    },
+    empty: function(){
+      console.log("You've called the \"empty\" function.");
+      let cart = this.get('auth.profile.cart');
+      cart.get("cartitemtypequantities").forEach(function(cartitemtypequantity){
+      cartitemtypequantity.deleteRecord();
+      cartitemtypequantity.save()
+      });
+    }
+  }
 });
--- app/controllers/library.js
+++ app/controllers/library.js
@@ -1,36 +1,22 @@
-import { computed } from '@ember/object';
-import { A } from '@ember/array';
 import Controller from '@ember/controller';
-// import { computed } from '@ember/object';
-import $ from 'jquery';
+
 export default Controller.extend({
 
-  listitem: computed('', function(){
-    return 'rpi2b.jpg';
-  }),
+  actions:{
+    add: function(itemtype, quantity){
+      let cart = this.get('auth.profile.cart');
+      console.log(cart);
+      console.log("You've called the \"add\" function.");
+      console.log('Itemtype: ' + itemtype);
+      console.log('Quantity: ' + quantity);
 
-  actions: {
-    getitemcount(){
-      //Loop through the objects and get count for each object and update objlist
-      this.objlist.forEach(function(ite){
-      $.ajax({
-        //url can change, we just need to update the url
-        url:'/api/items/count?partname='+ite.name,
-        type:"GET",
-        contentType:"application/json",
-        dataType:"json",
-        success: function(response){
-          ite.qty=response.count;
-        },
-        error: function() {
-          // Do error handling
-        }
+      let newitem = this.get('store').createRecord('cartitemtypequantity', {
+        cart: cart,
+        itemtype: itemtype,
+        quantity: quantity
       });
-    });
+
+      newitem.save();
+      }
   }
-},
-
-//When we fecth items, just call this.actions.getitemcount(), this will update quantity values in objlist.
-
-  objlist: A([{name:'item1', qty:'33' ,link:'rpi2b.jpg'}, {name:'item2', qty:'1' , link:'rpi2b.jpg'}]),
 });
